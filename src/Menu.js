feccf5402d8c359353fb40373231878f026e3607
Yago Oliveira <guito_flash@yahoo.com>
2024-11-19 17:17:48 -0300

    Inserção de um menu

---
+++ src/Menu.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Menu() {
+    return (
+        <div className='aluno-container'>
+
+        </div>
+    );
+}
+
+export default Menu;
